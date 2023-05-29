6d54ed7a7944bc112d47cd7badfc8b8122c9529c
gabrielberarducci <gabrielberarducci@gmail.com>
2023-05-29 14:47:44 +0200

    contact functionality working with fetch

--- Projects/Project1/resources/js/contact.js
+++ Projects/Project1/resources/js/contact.js
@@ -1,26 +1,33 @@
 /* Crea tu propia lógica para hacer un fetch que emule una post request a un servidor y enseñe un mensaje en consola cuando la llamada se resuelva */
 /*  ADVANCED: utiliza DOM manipulation para enseñarle al user que su mensaje se ha enviado correctamente o no */
 
-const form = document.getElementById("form"); 
-const formName = document.getElementById("name");
-const formEmail = document.getElementById("email");
-const formPhone = document.getElementById("phone");
-const formMessage = document.getElementById("message");
-const passedMessage = document.getElementById("SuceedMessage");
+const okMessage = document.getElementById("suceedMessage");
+const errorMessage = document.getElementById("errorMessage");
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-
-    console.log(`Hi my name is ${formName.value}, my email is ${formEmail.value} and my phone number is ${formPhone.value}. I have this message for you: ${formMessage.value}`);
-
-    passedMessage.hidden = false;
-
-    const myTimeoutId = setTimeout(() => {
-        passedMessage.hidden = true;
-    }, 4000);
-    
-    formName.value = "";
-    formEmail.value = "";
-    formPhone.value = "";
-    formMessage.value = "";
+    let dataForm = {
+        formName: document.getElementById("name").value,
+        formEmail: document.getElementById("email").value,
+        formPhone: document.getElementById("phone").value,
+        formMessage: document.getElementById("message").value
+    }
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(dataForm),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    })
+    .then(response => {
+            if (response.ok === true) {
+                okMessage.hidden = false;
+                setTimeout(() => okMessage.hidden = true, 4000);
+                document.getElementById("form").reset();
+            } else {
+                errorMessage.hidden = false;
+                setTimeout(() => errorMessage.hidden = true, 4000);
+            }
+            return response.json()})
+    .then(data => console.log(data))
 })
