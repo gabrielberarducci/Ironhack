97065f4a6000000b96dce0462eeaad33a16e0c3b
Gabriel <gabrielberarducci@gmail.com>
2023-05-30 21:12:49 +0200

    CSS started

--- Projects/Project1/resources/js/index.js
+++ Projects/Project1/resources/js/index.js
@@ -22,36 +22,11 @@ function scrollToTop() {
   });
 }
 
-
+  
 // create the articles for the Projects sections 
 getPosts().then((data) => {
-    let projectsTabsParent = document.querySelector(".articles-container");
-    newRandomChild(projectsTabsParent,data);
-    newRandomChild(projectsTabsParent,data);
-    newRandomChild(projectsTabsParent,data);
+  let projectsTabsParent = document.querySelector(".articles-container");
+  newRandomChild(projectsTabsParent,data);
+  newRandomChild(projectsTabsParent,data);
+  newRandomChild(projectsTabsParent,data);
 })
-
-
-// banner carousel with logos.
-$(document).ready(function(){
-    $('.logo-carousel').slick({
-    arrows: false,
-    infinite: true,   // Permite la navegación infinita
-    slidesToShow: 2,   // Muestra 4 logos a la vez
-    autoplay: true,   // Inicia la reproducción automática
-    autoplaySpeed: 0,   // Velocidad de reproducción automática en milisegundos
-    cssEase: "linear",   // Establece el efecto de movimiento continuo
-    speed: 4000,   // Velocidad del movimiento en milisegundos
-    centerMode: true,
-    draggable: true,
-    pauseOnHover: false,
-    responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2   // Cambia a mostrar 1 logo en dispositivos más pequeños
-          }
-        }
-    ]
-    });
-  });
