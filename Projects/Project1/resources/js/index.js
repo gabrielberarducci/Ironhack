6c02a7ac8167e2b747df1ccb0197386a2e90e7ad
Gabriel <gabrielberarducci@gmail.com>
2023-05-30 10:40:59 +0200

    carousel banner with logos OK

--- Projects/Project1/resources/js/index.js
+++ Projects/Project1/resources/js/index.js
@@ -6,3 +6,23 @@
     newRandomChild(projectsTabsParent,data);
     newRandomChild(projectsTabsParent,data);
 })
+
+
+$(document).ready(function(){
+    $('.logo-carousel').slick({
+    infinite: true,   // Permite la navegación infinita
+    slidesToShow: 4,   // Muestra 4 logos a la vez
+    autoplay: true,   // Inicia la reproducción automática
+    autoplaySpeed: 0,   // Velocidad de reproducción automática en milisegundos
+    cssEase: "linear",   // Establece el efecto de movimiento continuo
+    speed: 4000,   // Velocidad del movimiento en milisegundos
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2   // Cambia a mostrar 1 logo en dispositivos más pequeños
+          }
+        }
+    ]
+    });
+  });
