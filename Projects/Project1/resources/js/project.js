b2ac02c6da7d64aef69d939822257ee9412ee3d8
Gabriel <gabrielberarducci@gmail.com>
2023-06-15 19:32:31 +0200

    first commit week10

--- Projects/Project1/resources/js/project.js
+++ Projects/Project1/resources/js/project.js
@@ -2,31 +2,31 @@
 /* ADVANCED: consigue que la info del post funcione dinámicamente y enseñe un post u otro según la URL */
 
 
-function getPosts(){
-    return fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => {
-        if(response.ok)    
-            return response.json()
-        else
-            throw Error("Failed to get Posts data");
-    })
-    .catch((error) => {
-        console.log(error.message);
-    })
+async function getPosts() {
+    return await fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => {
+            if (response.ok)
+                return response.json();
+            else
+                throw Error("Failed to get Posts data");
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
 }
 
-function newRandomChild(parent, data, id = 0){ //el parametro id es opcional- 
+function newRandomChild(parent, data, id = 0){ //el parametro id es opcional, si no se incluye en la llamada, toma el valor "0" y no es tenido en cuenta al momento de ejecucion. 
     let newChild = document.createElement("div");
     newChild.className = "post";
     newChild = document.createElement("div");
     let i = Math.floor(Math.random() * 100);
-    if(id > 0) i=id-1; //permite de forma opcional elegir el articulo segun su ID. si no lo hace random.
+    if(id > 0) i=id-1; //permite de forma opcional elegir el articulo segun su ID. Si el valor id = "0", toma un valor random.
     newChild.innerHTML = `
     <div class="article" id="${data[i].id}">    
-    <img class="project-img" src="/Projects/Project1/resources/images/projects-section/${(i % 6) + 1}.jpg" alt="Project-image">
+    <img class="project-img" src="/resources/images/projects-section/${(i % 6) + 1}.jpg" alt="Project-image">
         <h2>${data[i].title}</h2>
         <p>${data[i].body}</p>
-        <a rel="stylesheet" href="http://127.0.0.1:5500/Projects/Project1/project.html?id=${data[i].id}">Learn More</a>
+        <a rel="stylesheet" href="/project.html?id=${data[i].id}">Learn More</a>
     </div> 
     `;
     parent.appendChild(newChild);
@@ -35,9 +35,11 @@ function newRandomChild(parent, data, id = 0){ //el parametro id es opcional-
 function newChildById(parent, data){
     // Obtener la URL actual
     var url = new URL(window.location.href);
-    // Obtener el valor de un parámetro específico
+    // Obtener el valor de un parámetro específico, en este caso "id"
     var id = url.searchParams.get('id');
+    // creamos un nuevo elemento. 
     let newChild = document.createElement("div");
+    // obtenemos la fecha de hoy para mostrarla en la pagina. 
     const todayDate = new Date();
     const options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
     const formattedDate = todayDate.toLocaleDateString('en-US', options);
@@ -45,22 +47,26 @@ function newChildById(parent, data){
     newChild.className = "post";
     newChild = document.createElement("div");
     newChild.innerHTML = `
-    <div id="${data[id-1].id}">    
+    <div id="${data[id-1].id}" class="mainArticle">    
         <h2>${data[id-1].title}</h2>
         <div class="subtitle">
             <p class="articleSubtitle">${data[id-1].title}</p>
             <p class="date">Completed on ${formattedDate}</p>
         </div>
-        <img src="/Projects/Project1/resources/images/projects-section/${((id-1) % 6) + 1}.jpg"
+        <img src="/resources/images/projects-section/${((id-1) % 6) + 1}.jpg"
         <p>${data[id-1].body}</p>
     </div> 
     `;
+    // Insertamos el nuevo elemento HTML en la pagina con la informacion obtenida. 
     parent.appendChild(newChild);
 }
 
-getPosts().then((data) => {
+getPosts().then((data) => { //Al ejecutar esta funcion, creamos todo el contenido necesario para la pagina de projects.html
     let projectsTabsParent = document.querySelector(".articles-container");
     let mainProjectParent = document.querySelector("#mainProject");
+    var url = new URL(window.location.href);
+    var id = url.searchParams.get('id');
+    if (id === null) return null; // de esta manera la funcion continua solo si estamos en project.html y no en index.html. 
     newChildById(mainProjectParent,data);
     newRandomChild(projectsTabsParent,data);
     newRandomChild(projectsTabsParent,data);
--- Projects/Project1/resources/js/splash.js
+++ Projects/Project1/resources/js/splash.js
@@ -1,1 +1,13 @@
-/* Crea tu propia lógica para hacer una splash page que desaparezca */
+/* Muestra una pantalla de carga al entra a cualquier pagina del sitio. 
+Tiene configurado un retardo de apenas 500ms para que el efecto sea apreciable
+por mas que la carga de la pagina muy demasiado rapida */
+
+// Loading effect
+window.addEventListener('load', function() {
+    const loading = document.querySelector('.loading');
+    this.setTimeout(() => {
+      // con esto ocultamos la ventana de loading luego de 500ms
+      // esto permite que se muestre momentaneamente el efecto de loading por mas que la pagina cargue demasiado rapido.
+      loading.style.display = 'none'; 
+    }, 500);
+  });
